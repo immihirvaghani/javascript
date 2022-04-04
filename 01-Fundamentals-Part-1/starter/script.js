861671585c00b776c4e1855bb3b06b4b5b1f98b1
immihirvaghani <mac@macs-MacBook-Air.local>
2022-04-04 11:04:34 +0530

    All files are updated

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,66 @@
+// Coding challenge #1 and #2
+// let markWeight = 78;
+// let markHeight = 1.69;
+
+// let weight = markWeight;
+// let height = markHeight;
+
+// const bmiMark = weight / height ** 2;
+// console.log(bmiMark);
+
+// let johnkWeight = 92;
+// let johnHeight = 1.95;
+
+// weight = johnkWeight;
+// height = johnHeight;
+
+// const bmiJohn = weight / height ** 2;
+// console.log(bmiJohn)
+
+// const markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
+
+// if(bmiMark > bmiJohn){
+//     console.log(`Mark's BMI is ${bmiMark} higher than John's!`);
+// } else{
+//     console.log('John\'s BMI is higher than Mark\'s!"');
+// }
+
+// const birthYear = '1991';
+// const birthYearNum = Number(birthYear);
+// console.log(birthYearNum + 18)
+
+// Coding challenge #3
+// const teamDolphinsR1 = 97;
+// const teamDolphinsR2 = 112;
+// const teamDolphinsR3 = 101;
+
+// const avgScoreDolphins = (teamDolphinsR1 + teamDolphinsR2 + teamDolphinsR3) / 3;
+// console.log(Math.round(avgScoreDolphins));
+
+// const teamKoalasR1 = 109;
+// const teamKoalasR2 = 95;
+// const teamKoalasR3 = 100;
+
+// const avgScoreKoalas = (teamKoalasR1 + teamKoalasR2 + teamKoalasR3) / 3;
+// console.log(Math.round(avgScoreKoalas));
+
+// let minimummScore = 100;
+
+// if(avgScoreDolphins >= minimummScore && avgScoreDolphins > avgScoreKoalas){
+//     console.log(`Team Dolphin's score is ${avgScoreDolphins}, Winners!`);
+// } else if (avgScoreKoalas >= minimummScore && avgScoreKoalas > avgScoreDolphins){
+//     console.log(`Team Koalas's score is ${avgScoreKoalas}, Winners!`);
+// } else if (avgScoreDolphins >= minimummScore, avgScoreKoalas >= minimummScore && avgScoreKoalas === avgScoreDolphins){
+//     console.log(`It's a draw`);
+// } else {
+//     console.log(`Score might be lower than ${minimummScore}`);
+// }
+
+// Coding challenge #4
+// let billAmt = 430;
+// let tip
+
+// const tipAmt = billAmt >= 50 && billAmt <= 300 ? tip = billAmt * 15 / 100 : tip = billAmt * 20 /100;
+
+// console.log(`Your Bill is ${billAmt} so your tip is ${tipAmt}, Total ${billAmt + tipAmt}.`)
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,100 @@
+// const cutPieces = function (fruit){
+//     return fruit * 4;
+// }
+
+// const fruitProcessor = function (apples, oranges){
+
+//     const applePieces = cutPieces(apples);
+//     const orangePieces = cutPieces(oranges);
+    
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces } oranges.`;
+//     return juice;
+// };
+
+// console.log(fruitProcessor(2, 3)) 
+
+// const calcAge = function(year){
+//     return 2022 - year; 
+// }
+
+// const yearsUntilRetirement = function(birthYear, firstName) {
+
+//     const age = calcAge(birthYear)
+//     const retirement = 65 - age;
+
+//     if(retirement > 0){
+//         console.log(`${firstName} retires in ${retirement} years.`);
+//         return retirement;
+//     } else{
+//         return -1;
+//     }
+//     // return 
+// }
+
+// console.log(yearsUntilRetirement(1999, 'Mihir'));
+
+
+// Challenge 1
+// const calcAverage = (round1, round2, round3) => {  
+//     return (round1 + round2 + round3) / 3;
+// }
+
+// const avgDolphins = calcAverage(85, 54, 41);
+// console.log(avgDolphins);
+// const avgKoalas = calcAverage(23, 34, 27);
+// console.log(avgKoalas);
+
+// const checkWinner = () => { 
+//     if(avgDolphins >= avgKoalas *2){
+//         console.log(`Dolphins win ${avgDolphins} vs. ${avgKoalas}.`)
+//     } else if(avgKoalas >= avgDolphins *2){
+//         console.log(`Koalas win ${avgKoalas} vs. ${avgDolphins}.`)
+//     } else{
+//         console.log(`it's draw`)
+//     }
+// }
+
+// checkWinner(avgDolphins, avgKoalas)
+
+// Challenge 2
+// let tipAmount;
+
+// const bills = [125, 555, 44];
+// let tips = [];
+
+// const calcTip = (billsAmount) => {
+//     if(billsAmount >= 50 && billsAmount <= 300){
+//         return tipAmount = billsAmount * 15 / 100;
+//     } else {
+//         return tipAmount = billsAmount * 20 / 100;
+//     }
+// }
+ 
+// console.log(bills)
+
+// tips.push(calcTip(bills[0]));
+// tips.push(calcTip(bills[1]));
+// tips.push(calcTip(bills[2]));
+// console.log(tips)
+
+// let totalBill = [];
+
+// totalBill.push(Math.floor(bills[0] + tips[0]));
+// totalBill.push(Math.floor(bills[1] + tips[1]));
+// totalBill.push(Math.floor(bills[2] + tips[2]));
+// console.log(totalBill);
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtman',
+    birthYear: 1999,
+    calcAge: function (){
+        return 2022 - this.birthYear;
+    },
+    job: 'Teacher',
+    friends: ['Michel', 'Peter', 'Stev']
+}
+
+console.log(jonas.calcAge())
+
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
