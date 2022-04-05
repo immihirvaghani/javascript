42b7de5efcb6e917f61372504621231ddcff2f47
immihirvaghani <immihir1292@gmail.com>
2022-04-05 18:18:45 +0530

    First project

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = (arr) => {
+    for( let i = 0; i < arr.length; i++){
+        console.log(`${arr[i]}oC in ${i} days`)
+    }
+}
+
+printForecast([12, 5, -5, 0, 4])
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0){
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = 'You Lose game!';
+    }
+  }
+});
