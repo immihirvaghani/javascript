8bb2f1ded9433c9ee3a49e9d81a5fdc352e7ab41
immihirvaghani <immihir1292@gmail.com>
2022-04-08 11:43:56 +0530

    Modal project completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtn = document.querySelectorAll('.show-modal');
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < showBtn.length; i++)
+    showBtn[i].addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal)
